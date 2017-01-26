f105eb157a8856b4084b49ca924c1a97472577cd
mrmedvedev <riffraff92@gmail.com>
2017-01-26 22:09:49 +0300

    les12

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -11,7 +12,7 @@ namespace Snake
         static void Main(string[] args)
         {
             Console.SetBufferSize(80, 25);
-
+            //Frame
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
@@ -25,13 +26,46 @@ namespace Snake
 
 
 
-
+            //Point
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+
+            FoodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if(snake.Eat( food) )
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+            }
+            while (true)
+            {
+
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
+            snake.Move();
 
            
 
-          Console.ReadLine();
+          
         }
        
     }
--- Snake/Snake/point.cs
+++ Snake/Snake/point.cs
@@ -20,6 +20,32 @@ namespace Snake
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move (int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y - offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y + offset;
+            }
+        }
 
 
        public void Draw()
@@ -27,6 +53,14 @@ namespace Snake
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
-
-     }
+         public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
 }

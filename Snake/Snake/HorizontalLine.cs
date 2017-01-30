72cadce183877bdc79970e5ba7e813fc9a0414d9
mrmedvedev <riffraff92@gmail.com>
2017-01-30 22:31:46 +0300

    game

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -6,20 +6,16 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine : Figure
-    {
-        
-        public HorizontalLine(int xLeft, int xRight, int y, char sym )
-        {
-            pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++)
-            {
-                Point p = new Point(x, y, sym);
-                pList.Add(p);
-                
-            }
-        
-        }
-
-    }
+	class HorizontalLine : Figure
+	{
+		public HorizontalLine(int xLeft, int xRight, int y, char sym)
+		{
+			pList = new List<Point>();
+			for(int x = xLeft; x <= xRight; x++)
+			{
+				Point p = new Point( x, y, sym );
+				pList.Add( p );
+			}			
+		}
+	}
 }
--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,20 +6,16 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine :Figure
-    {
-       
-        public VerticalLine(int yUp, int yDown, int x, char sym)
-        {
-            pList = new List<Point>();
-            for (int x = yUp; x <= yDown; x++)
-            {
-                Point p = new Point(x, y, sym);
-                pList.Add(p);
-
-            }
-
-        }
-        
-    }
+	class VerticalLine : Figure
+	{
+		public VerticalLine( int yUp, int yDown, int x, char sym )
+		{
+			pList = new List<Point>();
+			for(int y = yUp; y <= yDown; y++)
+			{
+				Point p = new Point( x, y, sym );
+				pList.Add( p );
+			}			
+		}
+	}
 }
